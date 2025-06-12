9ee2603ece4aa3717404807a9508e6ff74821800
Dmitrii <143752974+KidwellyBrand@users.noreply.github.com>
2025-06-12 23:17:06 +0300

    EDIT

---
+++ QRScannerAdd/MainRibbon.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace QRScannerAdd
+{
+    partial class MainRibbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public MainRibbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.group = this.Factory.CreateRibbonGroup();
+            this.Scan = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.group.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.group);
+            this.tab1.Label = "QR-Сканер";
+            this.tab1.Name = "tab1";
+            // 
+            // group
+            // 
+            this.group.Items.Add(this.Scan);
+            this.group.Label = "Дейсвие";
+            this.group.Name = "group";
+            // 
+            // Scan
+            // 
+            this.Scan.Label = "Сканировать";
+            this.Scan.Name = "Scan";
+            this.Scan.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.Scan_Click);
+            // 
+            // MainRibbon
+            // 
+            this.Name = "MainRibbon";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.MainRibbon_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.group.ResumeLayout(false);
+            this.group.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton Scan;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal MainRibbon MainRibbon
+        {
+            get { return this.GetRibbon<MainRibbon>(); }
+        }
+    }
+}
