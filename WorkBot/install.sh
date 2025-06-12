8e90f4cccec4aad3d73e3a0001a016f0a2d7e1e4
Dmitrii <143752974+KidwellyBrand@users.noreply.github.com>
2025-06-12 13:19:07 +0300

    edit

---
+++ WorkBot/install.sh
@@ -0,0 +1,15 @@
+﻿echo "WorkBot: Установка сервиса"
+sudo systemctl enable /usr/share/remeslo/WorkBot/WorkBot.service || true
+hostName=$(hostname | tr '[:lower:]' '[:upper:]')
+echo "WorkBot: сервер $hostName"
+if [ -f  /usr/share/remeslo/WorkBot/appsettings."$hostName".json ]
+then
+  echo "WorkBot: Копирование конфигурации для сервера $hostName"
+  sudo cp /usr/share/remeslo/WorkBot/appsettings."$hostName".json /usr/share/remeslo/WorkBot/appsettings.json
+else
+  echo "WorkBot: Копирование конфигурации по умолчанию"
+  sudo cp /usr/share/remeslo/WorkBot/appsettings.DEFAULT.json /usr/share/remeslo/WorkBot/appsettings.json
+fi
+echo "WorkBot: Запуск сервиса"
+sudo systemctl start WorkBot || true
+echo "WorkBot: Настройка завершена"
